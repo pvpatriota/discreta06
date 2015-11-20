6079ec0e812db5bace2605d791b45891c717f0d7
Paulo Vitor Alves Patriota <paulovitorpatriota@gmail.com>
2015-11-20 18:02:26 -0300

    Adicionado a biblioteca do allegro no código

--- ex6.c
+++ ex6.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <allegro.h>
+
+#define MAXPARAMETROS 6 // Define o número máximo dos parâmetros
+#define TAMANHO 40 // Define o diâmetro dos círculos
+#define XMAX 600
+#define YMAX 500
+#define EMAX 6 // Define o número de entradas
+
 
 int main(void)
 {
