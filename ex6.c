09be5cf037952a59b305369c89fe9073a4ef9ee1
Kewin Lima Da Silva <lima_kewin@hotmail.com>
2015-11-20 22:42:20 -0300

    Variaveis estaticas deletadas

--- ex6.c
+++ ex6.c
@@ -19,34 +19,6 @@ int main(void)
     int entrada[EMAX][3],cor[3],parametro[6],alfa;
     /* 0= origem, 1= letra, 2= destino */
 
-    parametro[0] = 5; // quantidade
-    parametro[1] = 2; // alfabeto (a,b)
-    parametro[2] = 0; // começo
-    parametro[3] = 0; //fim
-    parametro[4] = 3; //fim
-    parametro[5] = 4; //fim 
-
-    /* Entradas estáticas para testes, posteriormente serão removidas */
-
-    entrada[0][0] =0; 
-    entrada[0][1] =0;
-    entrada[0][2] =1;
-    entrada[1][0] =1;
-    entrada[1][1] =0;
-    entrada[1][2] =2;
-    entrada[2][0] =1;
-    entrada[2][1] =1;
-    entrada[2][2] =3;
-    entrada[3][0] =2;
-    entrada[3][1] =1;
-    entrada[3][2] =4;
-    entrada[4][0] =3;
-    entrada[4][1] =0;
-    entrada[4][2] =1;
-    entrada[5][0] =4;
-    entrada[5][1] =0;
-    entrada[5][2] =1;
-
     int n,eixoX[parametro[0]],eixoY[parametro[0]],tamanho = 50;
 
     FILE *arq= fopen("automato1.txt","r");
@@ -54,6 +26,7 @@ int main(void)
     fscanf(arq,"%d", &parametro[1]); //alfabeto
     fscanf(arq,"%d", &parametro[2]); //começo
     fscanf(arq,"%d %d %d", &parametro[3], &parametro[4], &parametro[5]);//fim
+    
     for(n=0;n<EMAX;n++)
     {
         fscanf(arq,"%d %d %d", &entrada[n][0],&entrada[n][1],&entrada[n][2]);
